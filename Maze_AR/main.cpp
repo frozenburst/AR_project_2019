9087f21ef1e713c8123ea33e08a19b14f5c7ff35
hsuanlyh1997 <tmpss1131@gmail.com>
2019-06-06 15:22:00 +0900

    finish fix the ball colliding bug

--- Maze_AR/main.cpp
+++ Maze_AR/main.cpp
@@ -149,21 +149,20 @@ void checkCollision_i(float add_v_i){
     float ballpos_up = ballpos_new_i+ball_radius;
     float ballpos_down = ballpos_new_i-ball_radius;
     
-    // cout << ballpos_up << ' ' << ballpos_down  << endl;
-    
-    
-    int j_idx = (ballpos[0] + 0.0135f) / 0.001f;
-    int i_idx_new = 0;
+    int j_idx = (int)(round((ballpos[0] + 0.0135f) / 0.001f));
+    int i_idx_new = (int)(round((ballpos_new_i - 0.0135f) / (-0.001f)));
     
     if(add_v_i < 0){//down
-        i_idx_new = (ballpos_down - 0.0135f) / (-0.001f);
+        i_idx_new = (int)(round((ballpos_down - 0.0135f) / (-0.001f)));
         
-        if(maze[j_idx][i_idx_new+1] == 1){
-            float upper_bound = 0.0135f - (i_idx_new+1)*(0.001f) + 0.0005f;
-            // cout << "upperbd " << upper_bound << ' ' << ballpos_down << endl;
+        if(maze[j_idx][i_idx_new] == 1){
+            float upper_bound = 0.0135f - (i_idx_new)*(0.001f) + 0.0005f;
+            if(balldebug)
+                cout << "upperbd " << upper_bound << ' ' << ballpos_down << endl;
             if(ballpos_down < upper_bound){
-                // cout << "i col (down dir)!" << endl;
-                ball_speed[1] = -0.1f*ball_speed[1];
+                if(balldebug)
+                    cout << "i col (down dir)!" << endl;
+                ball_speed[1] = -0.5f*ball_speed[1];
             }
             else{
                 ballpos[1] += add_v_i;
@@ -175,14 +174,16 @@ void checkCollision_i(float add_v_i){
         
     }
     else if(add_v_i > 0){//up
-        i_idx_new = (ballpos_up - 0.0135f) / (-0.001f);
+        i_idx_new = (int)(round((ballpos_up - 0.0135f) / (-0.001f)));
         
         if(maze[j_idx][i_idx_new] == 1){
             float lower_bound = 0.0135f - (i_idx_new)*(0.001f) - 0.0005f;
-            // cout << "lower_bound " << lower_bound << ' ' << ballpos_up << endl;
+            if(balldebug)
+                cout << "lower_bound " << lower_bound << " ballpos_up " << ballpos_up << " i_idx_new " << (ballpos_up - 0.0135f) / (-0.001f) << endl;
             if(ballpos_up > lower_bound){
-                // cout << "i col (up dir)!" << endl;
-                ball_speed[1] = -0.1f*ball_speed[1];
+                if(balldebug)
+                    cout << "i col (up dir)!" << endl;
+                ball_speed[1] = -0.5f*ball_speed[1];
             }
             else{
                 ballpos[1] += add_v_i;
@@ -193,9 +194,10 @@ void checkCollision_i(float add_v_i){
         }
         
     }
-
-    // cout << "i " << j_idx << ' ' << ballpos_new_i << ' ' << i_idx_new << endl;
-
+    
+    if(balldebug)
+        cout << "i: j_idx " << j_idx << ' ' << ballpos[0] << ' ' << (ballpos[0] + 0.0135f) / 0.001f << " i_idx " << i_idx_new << endl;
+    
 }
 
 
@@ -205,21 +207,20 @@ void checkCollision_j(float add_v_j){
     float ballpos_left = ballpos_new_j-ball_radius;
     float ballpos_right = ballpos_new_j+ball_radius;
     
-    cout << ballpos_left << ' ' << ballpos_right << endl;
-    
-    
-    int i_idx = (ballpos[1] - 0.0135f) / (-0.001f);
-    int j_idx_new = (ballpos_new_j + 0.0135f) / (0.001f);
+    int i_idx = (int)(round((ballpos[1] - 0.0135f) / (-0.001f)));
+    int j_idx_new = (int)(round((ballpos_new_j + 0.0135f) / (0.001f)));
     
     if(add_v_j < 0){//left
-        j_idx_new = (ballpos_left + 0.0135f) / (0.001f);
+        j_idx_new = (int)(round((ballpos_left + 0.0135f) / (0.001f)));
         
         if(maze[j_idx_new][i_idx] == 1 || j_idx_new == 0){
             float right_bound = -0.0135f + (j_idx_new)*(0.001f) + 0.0005f;
-            cout << "right_bound " << right_bound << ' ' << ballpos_left << endl;
+            if(balldebug)
+                cout << "right_bound " << right_bound << ' ' << ballpos_left << endl;
             if(ballpos_left < right_bound){
-                cout << "j col (left dir)!" << endl;
-                ball_speed[0] = -0.1f*ball_speed[0];
+                if(balldebug)
+                    cout << "j col (left dir)!" << endl;
+                ball_speed[0] = -0.5f*ball_speed[0];
             }
             else{
                 ballpos[0] += add_v_j;
@@ -231,14 +232,16 @@ void checkCollision_j(float add_v_j){
         
     }
     else if(add_v_j > 0){//right
-        j_idx_new = (ballpos_right + 0.0135f) / (0.001f);
+        j_idx_new = (int)(round((ballpos_right + 0.0135f) / (0.001f)));
         
-        if(maze[j_idx_new+1][i_idx] == 1){
-            float left_bound = -0.0135f + (j_idx_new+1)*(0.001f) - 0.0005f;
-            cout << "left_bound " << left_bound << ' ' << ballpos_right << endl;
+        if(maze[j_idx_new][i_idx] == 1){
+            float left_bound = -0.0135f + (j_idx_new)*(0.001f) - 0.0005f;
+            if(balldebug)
+                cout << "left_bound " << left_bound << ' ' << ballpos_right << endl;
             if(ballpos_right > left_bound){
-                cout << "j col (right dir)!" << endl;
-                ball_speed[0] = -0.1f*ball_speed[0];
+                if(balldebug)
+                    cout << "j col (right dir)!" << endl;
+                ball_speed[0] = -0.5f*ball_speed[0];
             }
             else{
                 ballpos[0] += add_v_j;
@@ -249,25 +252,32 @@ void checkCollision_j(float add_v_j){
         }
     }
     
-    // cout << "j "   << ballpos_new_j << ' ' << j_idx_new << ' '<< i_idx << endl;
-    
+    if(balldebug)
+        cout << "j "   << ballpos_new_j << ' ' << j_idx_new << ' '<< i_idx << endl;
 }
 
+
 void ball_movement() {
-	float framerate = 60.0f;
-
-	// manipulate the speed by keyboard
-	ball_speed[0] += ball_flag[0] * ball_speed_unit;
-	ball_speed[1] += ball_flag[1] * ball_speed_unit;
-
-	checkSpeedWithGravity();
+    float framerate = 60.0f;
     
+    // manipulate the speed by keyboard
+    ball_speed[0] += ball_flag[0] * ball_speed_unit;
+    ball_speed[1] += ball_flag[1] * ball_speed_unit;
+    
+    checkSpeedWithGravity();
+    
+    if(balldebug)
+        cout << "1-------------------------------------\n";
+    if(balldebug)
+        cout << "-j-----------------------------------\n";
     checkCollision_j(0.5f * ball_speed[0]/ framerate);
+    if(balldebug)
+        cout << "-i------------------------------------\n";
     checkCollision_i(0.5f * ball_speed[1]/ framerate);
-
-//    ballpos[2] += 0.5f * ball_speed[2] / framerate;
-
-
+    
+    //    ballpos[2] += 0.5f * ball_speed[2] / framerate;
+    if(balldebug)
+        cout << "2-------------------------------------\n"; 
 }
 
 
