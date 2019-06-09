5fe499ab55c2f36e3dde36df77793c81201261a8
hsuanlyh1997 <tmpss1131@gmail.com>
2019-06-09 16:00:15 +0900

    modify random seed for generating maze

--- Maze_AR/DrawPrimitives.h
+++ Maze_AR/DrawPrimitives.h
@@ -4,13 +4,28 @@
 //#include <GL/gl.h>
 
 #include <math.h>
-
+#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
 
 /* PI */
 #ifndef M_PI
 #define M_PI 3.1415926535897932384626433832795
 #endif
 
+char b = char(219);
+int w = 28; //width
+int h = 28; //height
+vector < vector < int > > arr = {};
+vector < vector < int > > arr2 = {};
+vector < string > arr3 = {};
+vector < vector < int > > neigh = {};
+int r = 1; //row
+int c = 1; //column
+vector < int > nxt = {};
+int maze_create_cnt = 1;
 
 void drawSphere(double r, int lats, int longs) {
 	int i, j;
@@ -91,64 +106,33 @@ void drawSnowman( bool female = 0)
 	solidCone( 0.1, 0.3, 10, 10 );
 }
 // Added in Exercise 9 - End *****************************************************************
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-using namespace std;
-
-char b = char(219);
-int w = 28; //width
-int h = 28; //height
-vector < vector < int > > arr = {};
-vector < vector < int > > arr2 = {};
-vector < string > arr3 = {};
-vector < vector < int > > neigh = {};
-int r = 1; //row
-int c = 1; //column
-vector < int > nxt = {};
-int maze_create_cnt = 1;
 
 void create() {
     neigh.clear();
     //== counter to end recursive call quickly
-    if (maze_create_cnt >= (h / 2) * (w / 2)) {
-        return;
-    }
+    if (maze_create_cnt >= (h / 2) * (w / 2)) return;
+    
     //==== getting the unvisited neighbors
     if (r + 2 < h - 1 && arr[r + 2][c] == 0) {
-        neigh.push_back({
-            r + 2,
-            c
-        }); //down
+        neigh.push_back({r + 2, c}); //down
     }
     if (r - 2 > 0 && arr[r - 2][c] == 0) {
-        neigh.push_back({
-            r - 2,
-            c
-        }); //up
+        neigh.push_back({r - 2, c}); //up
     }
     if (c + 2 < w - 1 && arr[r][c + 2] == 0) {
-        neigh.push_back({
-            r,
-            c + 2
-        }); //right
+        neigh.push_back({r, c + 2}); //right
     }
     if (c - 2 > 0 && arr[r][c - 2] == 0) {
-        neigh.push_back({
-            r,
-            c - 2
-        }); //left
+        neigh.push_back({r, c - 2}); //left
     }
     //=== forward
     if (neigh.size() > 0) {
         nxt.clear();
         nxt = neigh[rand() % neigh.size()];
-        //mark it as visited
-        arr[nxt[0]][nxt[1]] = 2;
-        //counter to know if all cells are already visited
-        maze_create_cnt++;
-        //push to the stack
-        arr2.push_back(nxt);
+        arr[nxt[0]][nxt[1]] = 2; //mark it as visited
+        maze_create_cnt++; //counter to know if all cells are already visited
+        arr2.push_back(nxt); //push to the stack
+        
         //remove the walls
         if (c - nxt[1] < 0) {
             arr[r][c + 1] = 0;
@@ -167,7 +151,6 @@ void create() {
             r -= 2;
             create();
         }
-        
     }
     //==== backtrack
     else if (arr2.size() > 0) {
@@ -179,16 +162,11 @@ void create() {
 }
 
 vector < vector < int > > generate_maze() {
-    
     //adjusting the size
-    if (h % 2 == 0) {
-        h++;
-    }
-    if (w % 2 == 0) {
-        w++;
-    }
+    if (h % 2 == 0) h++;
+    if (w % 2 == 0) w++;
     
-    srand(407);
+    srand((unsigned int)(time(NULL)));
     
     //===== creating the base of the maze
     arr.resize(h);
@@ -204,7 +182,6 @@ vector < vector < int > > generate_maze() {
             }
         }
     }
-    
     //==== starting point of maze creation using depth-first search and recursive backtracking
     arr[r][c] = 2;
     arr2.push_back({r, c});
@@ -218,8 +195,8 @@ vector < vector < int > > generate_maze() {
 void drawCuboid(float width, float height, float depth)
 {
     glBegin(GL_QUADS);
-    //前
     
+    //前
     glNormal3f(0.0,0.0,-1.0);
     glVertex3f(width/2,height/2,depth/2);
     glVertex3f(-width/2,height/2,depth/2);
@@ -234,17 +211,13 @@ void drawCuboid(float width, float height, float depth)
     glVertex3f(width/2,-height/2,depth/2);
     
     //右
-    
     glNormal3f(-1.0,0.0,0.0);
     glVertex3f(-width/2,height/2,-depth/2);
     glVertex3f(-width/2,height/2,depth/2);
     glVertex3f(-width/2,-height/2,depth/2);
     glVertex3f(-width/2,-height/2,-depth/2);
     
-    
-    
     //上
-    
     glNormal3f(0.0,1.0,0.0);
     glVertex3f(width/2,height/2,depth/2);
     glVertex3f(-width/2,height/2,depth/2);
@@ -294,152 +267,5 @@ void drawMaze(vector<vector<int>>& maze){
                 glPopMatrix();
             }
         }
-
     }
-
-
 }
-//void drawMaze(){
-//    // base
-//    glColor4f(1,1,1,1);
-//    drawCuboid(0.028f, 0.028f, 0.001f);
-//
-//    // 1
-//    glPushMatrix();
-//    glColor4f(1,0,0,1);
-//    glTranslatef( 0.0135f, -0.0f, 0.001f );
-//    drawCuboid(0.001f, 0.028f, 0.001f);
-//    glPopMatrix();
-//
-//    // 3
-//    glPushMatrix();
-//    glTranslatef( -0.0135f, -0.0f, 0.001f );
-//    drawCuboid(0.001f, 0.028f, 0.001f);
-//    glPopMatrix();
-//
-//    // 2
-//    glPushMatrix();
-//    glTranslatef( -0.0f, 0.0135f, 0.001f );
-//    drawCuboid(0.026f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 4
-//    glPushMatrix();
-//    glTranslatef( -0.001f, -0.0135f, 0.001f );
-//    drawCuboid(0.024f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 5
-//    glPushMatrix();
-//    glTranslatef( 0.0105f, -0.01f, 0.001f );
-//    drawCuboid(0.001f, 0.006f, 0.001f);
-//    glPopMatrix();
-//
-//    // 6
-//    glPushMatrix();
-//    glTranslatef( 0.008f, -0.0075f, 0.001f );
-//    drawCuboid(0.004f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 7
-//    glPushMatrix();
-//    glTranslatef( 0.0105f, 0.0025f, 0.001f );
-//    drawCuboid(0.001f, 0.015f, 0.001f);
-//    glPopMatrix();
-//
-//    // 8
-//    glPushMatrix();
-//    glTranslatef( 0.0085f, -0.0045f, 0.001f );
-//    drawCuboid(0.003f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 9
-//    glPushMatrix();
-//    glTranslatef( -0.0f, 0.0095f, 0.001f );
-//    drawCuboid(0.020f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 10
-//    glPushMatrix();
-//    glTranslatef( -0.0105f, -0.0005f, 0.001f );
-//    drawCuboid(0.001f, 0.021f, 0.001f);
-//    glPopMatrix();
-//
-//    // 11
-//    glPushMatrix();
-//    glTranslatef( -0.0f, -0.0105f, 0.001f );
-//    drawCuboid(0.016f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 12
-//    glPushMatrix();
-//    glTranslatef( -0.003f, -0.0075f, 0.001f );
-//    drawCuboid(0.014f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 13
-//    glPushMatrix();
-//    glTranslatef( -0.0025f, -0.0045f, 0.001f );
-//    drawCuboid(0.015f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 14
-//    glPushMatrix();
-//    glTranslatef( 0.0075f, 0.0015f, 0.001f );
-//    drawCuboid(0.001f, 0.011f, 0.001f);
-//    glPopMatrix();
-//
-//    // 15
-//    glPushMatrix();
-//    glTranslatef( 0.0045f, 0.0025f, 0.001f );
-//    drawCuboid(0.001f, 0.009f, 0.001f);
-//    glPopMatrix();
-//
-//    // 16
-//    glPushMatrix();
-//    glTranslatef( 0.0015f, -0.002f, 0.001f );
-//    drawCuboid(0.001f, 0.004f, 0.001f);
-//    glPopMatrix();
-//
-//    // 17
-//    glPushMatrix();
-//    glTranslatef( 0.0015f, 0.006f, 0.001f );
-//    drawCuboid(0.001f, 0.006f, 0.001f);
-//    glPopMatrix();
-//
-//    // 18
-//    glPushMatrix();
-//    glTranslatef( 0.001f, 0.0005f, 0.001f );
-//    drawCuboid(0.006f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 19
-//    glPushMatrix();
-//    glTranslatef( -0.0015f, 0.004f, 0.001f );
-//    drawCuboid(0.001f, 0.006f, 0.001f);
-//    glPopMatrix();
-//
-//    // 20
-//    glPushMatrix();
-//    glTranslatef( -0.0045f, 0.007f, 0.001f );
-//    drawCuboid(0.001f, 0.005f, 0.001f);
-//    glPopMatrix();
-//
-//    // 21
-//    glPushMatrix();
-//    glTranslatef( -0.0045f, -0.0f, 0.001f );
-//    drawCuboid(0.001f, 0.004f, 0.001f);
-//    glPopMatrix();
-//
-//    // 22
-//    glPushMatrix();
-//    glTranslatef(-0.006f,  -0.0015f, 0.001f );
-//    drawCuboid(0.002f, 0.001f, 0.001f);
-//    glPopMatrix();
-//
-//    // 23
-//    glPushMatrix();
-//    glTranslatef( -0.0075f, 0.0015f, 0.001f );
-//    drawCuboid(0.001f, 0.011f, 0.001f);
-//    glPopMatrix();
-//}
